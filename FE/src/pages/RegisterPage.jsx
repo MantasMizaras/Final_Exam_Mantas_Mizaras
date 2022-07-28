faa92cf6e3c194a07b235983825af380d3d5cbe0
Mantas M <mantasef@gmail.com>
2022-07-28 18:26:16 +0300

    RegisterForm created, getting values in both Forms

--- FE/src/pages/RegisterPage.jsx
+++ FE/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import RegisterForm from '../components/RegisterForm/RegisterForm';
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return <RegisterForm />;
 }
 
 export default RegisterPage;
