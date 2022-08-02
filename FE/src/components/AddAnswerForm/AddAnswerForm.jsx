2b2886e5c67ba78f6d9d4efa5412dcb2488c70a0
Mantas M <mantasef@gmail.com>
2022-08-02 15:26:07 +0300

    AddAnswerForm moved to AnswerPage

--- FE/src/components/AddAnswerForm/AddAnswerForm.jsx
+++ FE/src/components/AddAnswerForm/AddAnswerForm.jsx
@@ -1,73 +1,67 @@
-import css from './AddAnswerForm.module.css';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { baseUrl, myFetchAuth } from '../../utils';
-import toast from 'react-hot-toast';
-import Button from '../UI/Button/Button';
-import { NavLink, useHistory, useParams } from 'react-router-dom';
-import { useAuthCtx } from '../../store/AuthContext';
-// import { getAnswers } from '../../../../src/controllers/answerController';
+// import css from './AddAnswerForm.module.css';
+// import { useFormik } from 'formik';
+// import * as Yup from 'yup';
+// import { baseUrl, myFetch, myFetchAuth } from '../../utils';
+// import toast from 'react-hot-toast';
+// import Button from '../UI/Button/Button';
+// import { NavLink, useHistory, useParams } from 'react-router-dom';
+// import { useAuthCtx } from '../../store/AuthContext';
 
-const initValues = {
-  answer: '',
-};
+// const initValues = {
+//   answer: '',
+// };
 
-function AddAnswerForm() {
-  const history = useHistory();
-  const { token } = useAuthCtx();
-  const { id } = useParams();
-  // const [answers, setAnswers] = useState([]);
+// function AddAnswerForm() {
+// const history = useHistory();
+// const { token } = useAuthCtx();
+// const { id } = useParams();
 
-  // const getAnswers = async (values) => {
-  //   const fetchResult = await myFetch(`${baseUrl}/api/question/${id}/answer`, 'GET', values);
-  //   if (Array.isArray(fetchResult)) {
-  //     setAnswers(fetchResult);
-  //   }
-  // };
+// const formik = useFormik({
+//   initialValues: initValues,
+//   validationSchema: Yup.object({
+//     answer: Yup.string().min(10, 'At least 10 symbols are required').max(555, 'Up to 555 symbols are allowed').required(),
+//   }),
+//   onSubmit: async (values, actions) => {
+//     // kvieti funkcija is AddAnswerPage, kuri daro fetch AnswerPage
+//     const addFetch = await myFetchAuth(`${baseUrl}/api/question/${id}/answer`, 'POST', token, values);
+//     if (addFetch === 'Answer succesfully added!') {
+//       toast.success('Your answer has been added!');
+//       history.go(0);
+//       // getAnswers();
+//       // actions.resetForm();
+//     }
+//     if (addFetch === 'Answer was not added!') {
+//       toast.error('Error while adding an answer. Please try again.');
+//       return;
+//     }
+//   },
+// });
 
-  const formik = useFormik({
-    initialValues: initValues,
-    validationSchema: Yup.object({
-      answer: Yup.string().min(10, 'At least 10 symbols are required').max(555, 'Up to 555 symbols are allowed').required(),
-    }),
-    onSubmit: async (values) => {
-      const addFetch = await myFetchAuth(`${baseUrl}/api/question/${id}/answer`, 'POST', token, values);
-      if (addFetch === 'Answer succesfully added!') {
-        toast.success('Your answer has been added!');
-        history.go(0);
-      }
-      if (addFetch === 'Answer was not added!') {
-        toast.error('Error while adding an answer. Please try again.');
-        return;
-      }
-    },
-  });
+// return (
+// <div className={css['form-container']}>
+//   <h1>Answer</h1>
 
-  return (
-    <div className={css['form-container']}>
-      <h1>Answer</h1>
+//   <form onSubmit={formik.handleSubmit}>
+//     <div className={css['form-group']}>
+//       <label htmlFor='answer'>Answer</label>
+//       <input
+//         onChange={formik.handleChange}
+//         onBlur={formik.handleBlur}
+//         value={formik.values.answer}
+//         className={formik.touched.answer && formik.errors.answer ? css['is-invalid'] : ''}
+//         type='answer'
+//         id='answer'
+//         name='answer'
+//       />
+//       {formik.touched.answer && formik.errors.answer && <p className={css['invalid-feedback']}>{formik.errors.answer}</p>}
+//     </div>
+//     <Button submit>Add</Button>
+//     <NavLink to={'/'} className={css['nav-link']}>
+//       Back to Questions
+//     </NavLink>
+//   </form>
+// </div>
+// );
+// }
 
-      <form onSubmit={formik.handleSubmit}>
-        <div className={css['form-group']}>
-          <label htmlFor='answer'>Answer</label>
-          <input
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.answer}
-            className={formik.touched.answer && formik.errors.answer ? css['is-invalid'] : ''}
-            type='answer'
-            id='answer'
-            name='answer'
-          />
-          {formik.touched.answer && formik.errors.answer && <p className={css['invalid-feedback']}>{formik.errors.answer}</p>}
-        </div>
-        <Button submit>Add</Button>
-        <NavLink to={'/'} className={css['nav-link']}>
-          Back to Questions
-        </NavLink>
-      </form>
-    </div>
-  );
-}
-
-export default AddAnswerForm;
+// export default AddAnswerForm;
--- FE/src/pages/AnswerPage/AnswerPage.jsx
+++ FE/src/pages/AnswerPage/AnswerPage.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 import { baseUrl, myDeleteAuth, myFetch, myFetchAuth } from '../../utils';
 import { useAuthCtx } from '../../store/AuthContext';
-import { useHistory, useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import AnswerCard from '../../components/UI/Answer/Answer';
 import css from '../AnswerPage/AnswerPage.module.css';
-import AddAnswerForm from '../../components/AddAnswerForm/AddAnswerForm';
 import toast from 'react-hot-toast';
 import Button from '../../components/UI/Button/Button';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 
 function AnswerPage() {
+  const initValues = {
+    answer: '',
+  };
+
   const title = localStorage.getItem('title');
   const content = localStorage.getItem('content');
 
@@ -18,15 +23,48 @@ function AnswerPage() {
   //   setUpdatedTitle({ title, content });
   // }, [title, content]);
 
-  //   const history = useHistory();
+  // const initValues = {
+  //   title: title,
+  //   content: content,
+  // };
+
+  // const [input, setIinput] = useState('');
+  // const [updatedTitleAndContent, setUpdatedTitleAndContent] = useState({ title, content });
+  // localStorage.removeItem('content');
+
+  // const [updatedTitle, setUpdatedTitle] = useState({ title, content });
+
+  // useEffect(() => {
+  //   setUpdatedTitle({ title, content });
+  // }, [title, content]);
+
+  // const history = useHistory();
   const { token } = useAuthCtx();
   const { id } = useParams();
   //   if (!token) history.push('/login');
-  const [answers, setAnswers] = useState([]);
+  const [answers, setAnswers] = useState('');
 
-  console.log('id, title, content  ===', id, title, content);
+  console.log('title ===', title);
+  console.log('content ===', content);
 
-  // const questTitle = title;
+  const formik = useFormik({
+    initialValues: initValues,
+    validationSchema: Yup.object({
+      answer: Yup.string().min(10, 'At least 10 symbols are required').max(555, 'Up to 555 symbols are allowed').required(),
+    }),
+    onSubmit: async (values, actions) => {
+      const addFetch = await myFetchAuth(`${baseUrl}/api/question/${id}/answer`, 'POST', token, values);
+      if (addFetch === 'Answer succesfully added!') {
+        toast.success('Your answer has been added!');
+        getAnswers();
+        actions.resetForm();
+      }
+      if (addFetch === 'Answer was not added!') {
+        toast.error('Error while adding an answer. Please try again.');
+        return;
+      }
+    },
+  });
 
   const getAnswers = async (values) => {
     const fetchResult = await myFetch(`${baseUrl}/api/question/${id}/answer`, 'GET', values);
@@ -62,6 +100,17 @@ function AnswerPage() {
     }
   };
 
+  const [updatedTitle, setUpdatedTitle] = useState({ title, content });
+
+  useEffect(() => {
+    setUpdatedTitle({ title, content });
+  }, [title, content]);
+
+  // useEffect(() => {
+  //   // setUpdatedTitleAndContent({ title: localStorage.getItem('title', 'content') });
+  //   setUpdatedTitleAndContent({ title, content });
+  // }, [title, content]);
+
   useEffect(() => {
     getAnswers();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -70,8 +119,8 @@ function AnswerPage() {
   // if (answers.length !== 0) {
   return (
     <div className={css['container']}>
-      <h3 className={css['title']}>Question: {title}</h3>
-      <p>Content: {content}</p>
+      <h3 className={css['title']}>Question:{updatedTitle.title}</h3>
+      <p>Content: {updatedTitle.content}</p>
 
       <div>
         <h3>Sort by creating time </h3>
@@ -85,7 +134,30 @@ function AnswerPage() {
           <p>No answers yet.</p>
         )}
       </div>
-      <AddAnswerForm onSubmit={getAnswers} />
+      {/* <AddAnswerForm onSubmit={getAnswers} /> */}
+      <div className={css['form-container']}>
+        <h1>Answer</h1>
+
+        <form onSubmit={formik.handleSubmit}>
+          <div className={css['form-group']}>
+            <label htmlFor='answer'>Answer</label>
+            <input
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.answer}
+              className={formik.touched.answer && formik.errors.answer ? css['is-invalid'] : ''}
+              type='answer'
+              id='answer'
+              name='answer'
+            />
+            {formik.touched.answer && formik.errors.answer && <p className={css['invalid-feedback']}>{formik.errors.answer}</p>}
+          </div>
+          <Button submit>Add</Button>
+          <NavLink to={'/'} className={css['nav-link']}>
+            Back to Questions
+          </NavLink>
+        </form>
+      </div>
     </div>
   );
   // } else {
